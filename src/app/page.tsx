ac2c5c69943f215fdcf89a83a2efda86490fafd7
devxiy <dev@xiy.com.ec>
2025-11-20 18:05:25 -0500

    Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import CatalogModule from '../components/CatalogModule';
 
 export default function Page() {
   return (
-    <main className="bg-[#05000D] text-white font-gabarito min-h-screen">
+    <main className="text-white font-gabarito min-h-screen">
       <CatalogModule />
       </main>
   );
