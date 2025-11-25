4f25884441ed1847c544bd8a1a8fc2b7fb708f4b
devxiy <dev@xiy.com.ec>
2025-11-25 09:49:49 -0500

    Update Hero

--- src/components/HeroSplit.tsx
+++ src/components/HeroSplit.tsx
@@ -13,7 +13,7 @@ const HeroSplit = ({ items }: { items: HeroPromo[] }) => {
           style={{ borderColor: card.border }}
         >
           <div className="">
-            <Image src={card.image} alt={card.title} className="relative" sizes="(max-width: 768px) 100vw, 720px" />
+            <Image src={card.image} alt={card.title} className="relative w-full" sizes="(max-width: 768px) 100vw, 720px" />
           </div>
         </article>
       ))}
