608c48a79805afb36386451db400ac7d625df7f6
devxiy <dev@xiy.com.ec>
2025-11-27 16:10:31 -0500

    Update Info

--- src/components/CatalogModule.tsx
+++ src/components/CatalogModule.tsx
@@ -52,7 +52,7 @@ const CatalogModule = () => {
         background: 'linear-gradient(180deg, #10001A 0%, #05000D 50%, #05000D 100%)',
       }}
     >
-      <div className="max-w-[1400px] mx-auto px-4 lg:px-4 py-4 space-y-4">
+      <div className="max-w-[1400px] mx-auto px-4 lg:px-4 py-0 space-y-4">
         {/* Header con categorías */}
         <NeonHeader />
 
